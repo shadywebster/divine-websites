38cafebf62d95fdd76740e582dff3fcdc8b05119
shadywebster <balajivtnj07@gmail.com>
2025-12-14 16:00:26 +0530

    Add Home component with Navigation and HeroSection

---
+++ page5.tsx
@@ -0,0 +1,13 @@
+import Navigation from '@/components/Navigation'
+import ScrollToTop from '@/components/ScrollToTop'
+import HeroSection from '@/components/sections/HeroSection'
+
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+      <HeroSection />
+      <ScrollToTop />
+    </>
+  )
+}
