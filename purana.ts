9a16da67e62e61a388897349276c28a27f72379b
shadywebster <balajivtnj07@gmail.com>
2025-12-14 16:13:21 +0530

    Create purana.ts

---
+++ purana.ts
@@ -0,0 +1,166 @@
+import { Purana } from '@/types';
+
+export const puranas: Purana[] = [
+  {
+    id: 'brahma',
+    name: 'Brahma Purana',
+    sanskritName: 'ब्रह्म पुराण',
+    description: 'The first of the eighteen Puranas, dedicated to Brahma',
+    details: 'The Brahma Purana is one of the eighteen major Puranas genre of Hindu texts in Sanskrit language. It is listed as the first Maha-Purana in all the anthologies, and therefore also called Adi Purana. The text is named after one of the cosmological theories of Hinduism, namely Brahma.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/bp/'
+  },
+  {
+    id: 'padma',
+    name: 'Padma Purana',
+    sanskritName: 'पद्म पुराण',
+    description: 'The Lotus Purana, containing stories of creation and various deities',
+    details: 'The Padma Purana is one of the eighteen major Puranas, a genre of texts in Hinduism. It is an encyclopedic text, named after the lotus in which creator god Brahma appeared. The text exists in many versions, one of which is organized into five parts.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/padma/'
+  },
+  {
+    id: 'vishnu',
+    name: 'Vishnu Purana',
+    sanskritName: 'विष्णु पुराण',
+    description: 'One of the most important Puranas, dedicated to Vishnu',
+    details: 'The Vishnu Purana is one of the eighteen Mahapuranas, a genre of ancient and medieval texts of Hinduism. It is an important Pancharatra text in the Vaishnavism tradition. The text presents cosmology, mythology, genealogy, geography, and religious practices.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/vp/'
+  },
+  {
+    id: 'shiva',
+    name: 'Shiva Purana',
+    sanskritName: 'शिव पुराण',
+    description: 'The Purana dedicated to Lord Shiva and his various forms',
+    details: 'The Shiva Purana is one of eighteen major texts of the Purana genre of Sanskrit texts in Hinduism, and part of the Shaivism literature corpus. It primarily centers around the Hindu god Shiva and goddess Parvati, but references and reveres all gods.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/sp/'
+  },
+  {
+    id: 'bhagavata',
+    name: 'Bhagavata Purana',
+    sanskritName: 'भागवत पुराण',
+    description: 'The most popular Purana, containing the life of Krishna',
+    details: 'The Bhagavata Purana, also known as the Srimad Bhagavatam, is one of Hinduism\'s eighteen great Puranas. Composed in Sanskrit and available in almost all Indian languages, it promotes bhakti (devotion) to Krishna integrating themes from the Advaita (monism) philosophy and theistic devotion.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/sb/'
+  },
+  {
+    id: 'narada',
+    name: 'Narada Purana',
+    sanskritName: 'नारद पुराण',
+    description: 'The Purana narrated by Sage Narada',
+    details: 'The Narada Purana is one of the major eighteen Mahapuranas, a genre of Hindu religious texts. It deals with the places of pilgrimages. It is in the style of a dialogue between the sage Narada and Sanatkumara.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/np/'
+  },
+  {
+    id: 'markandeya',
+    name: 'Markandeya Purana',
+    sanskritName: 'मार्कण्डेय पुराण',
+    description: 'The Purana containing the Devi Mahatmya',
+    details: 'The Markandeya Purana is one of the eighteen major Puranas. It is written in the style of a dialogue between the ancient sage Markandeya and Jaimini, a disciple of Vyasa. It contains the Devi Mahatmya, an important text for Shaktism.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/mp/'
+  },
+  {
+    id: 'agni',
+    name: 'Agni Purana',
+    sanskritName: 'अग्नि पुराण',
+    description: 'The Purana of fire, containing encyclopedic knowledge',
+    details: 'The Agni Purana is one of the eighteen major Puranas. The text is named after Agni, the Hindu god of fire. It is an encyclopedic text, covering topics such as cosmology, mythology, genealogy, politics, education system, iconography, taxation theories, organization of army, theories on proper causes for war, diplomacy, local laws, building public projects, water distribution methods, trees and plants, medicine, Vastu Shastra, gemology, grammar, metrics, poetry, food and agriculture, rituals, geography and travel guide to Mithila, cultural history, and more.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/ag/'
+  },
+  {
+    id: 'bhavishya',
+    name: 'Bhavishya Purana',
+    sanskritName: 'भविष्य पुराण',
+    description: 'The Purana of future predictions',
+    details: 'The Bhavishya Purana is one of the eighteen major works in the Purana genre of Hinduism. The text exists in many versions, and the title Bhavishya means "future" and implies it is a work that contains prophecies regarding the future.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/bhp/'
+  },
+  {
+    id: 'brahmavaivarta',
+    name: 'Brahmavaivarta Purana',
+    sanskritName: 'ब्रह्मवैवर्त पुराण',
+    description: 'The Purana describing the divine play of Radha and Krishna',
+    details: 'The Brahmavaivarta Purana is one of the eighteen major Puranas. It is divided into four parts. The first part describes the creation of the universe, the second part relates to description and histories of different goddesses, the third part describes the life of Krishna, and the fourth part describes the Kashi Khanda.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/bvp/'
+  },
+  {
+    id: 'linga',
+    name: 'Linga Purana',
+    sanskritName: 'लिंग पुराण',
+    description: 'The Purana dedicated to the Lingam form of Shiva',
+    details: 'The Linga Purana is one of the eighteen Mahapuranas, and a Shaivism text of Hinduism. The text describes the origin of the universe, the origin of the lingam, and related stories. It also contains many stories of lingam, one of which entails how Agni Lingam solved a dispute between Vishnu and Brahma.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/lp/'
+  },
+  {
+    id: 'varaha',
+    name: 'Varaha Purana',
+    sanskritName: 'वराह पुराण',
+    description: 'The Purana dedicated to Varaha, the boar avatar of Vishnu',
+    details: 'The Varaha Purana is one of the eighteen Mahapuranas, a genre of ancient and medieval texts of Hinduism. It is an important Vaishnavism text. The text is named after Varaha, the boar avatar of Vishnu.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/vp/'
+  },
+  {
+    id: 'skanda',
+    name: 'Skanda Purana',
+    sanskritName: 'स्कन्द पुराण',
+    description: 'The largest Purana, dedicated to Skanda (Kartikeya)',
+    details: 'The Skanda Purana is the largest Mahāpurāṇa, a genre of eighteen Hindu religious texts. The text is devoted mainly to the life and deeds of Skanda (also called Kartikeya, the son of Shiva and Parvati). The text contains over 81,000 verses, and is part of Shaivite literature.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/sp/'
+  },
+  {
+    id: 'vamana',
+    name: 'Vamana Purana',
+    sanskritName: 'वामन पुराण',
+    description: 'The Purana dedicated to Vamana, the dwarf avatar of Vishnu',
+    details: 'The Vamana Purana is a medieval era Sanskrit text and one of the eighteen major Puranas of Hinduism. The text is named after Vamana, the dwarf avatar of Vishnu. It is primarily a Vaishnavism text, and its first part glorifies Vishnu and his avatars such as Krishna.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/vp/'
+  },
+  {
+    id: 'kurma',
+    name: 'Kurma Purana',
+    sanskritName: 'कूर्म पुराण',
+    description: 'The Purana dedicated to Kurma, the tortoise avatar of Vishnu',
+    details: 'The Kurma Purana is one of the eighteen Mahapuranas, and a Vaishnavism text of Hinduism. The text is named after the tortoise avatar of Vishnu. The Kurma Purana, like other Puranas, includes legends, mythology, geography, manvantaras, and other topics.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/kp/'
+  },
+  {
+    id: 'matsya',
+    name: 'Matsya Purana',
+    sanskritName: 'मत्स्य पुराण',
+    description: 'The Purana dedicated to Matsya, the fish avatar of Vishnu',
+    details: 'The Matsya Purana is one of the eighteen major Puranas, and among the oldest and better preserved in the Puranic genre of Sanskrit literature. The text is a Vaishnavism text named after the fish avatar of Vishnu. The Matsya Purana has survived into the modern era in many versions.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/mp/'
+  },
+  {
+    id: 'garuda',
+    name: 'Garuda Purana',
+    sanskritName: 'गरुड़ पुराण',
+    description: 'The Purana containing details about death and afterlife',
+    details: 'The Garuda Purana is one of 18 Mahāpurāṇa texts in Hinduism. It is a part of Vaishnavism literature corpus, primarily centering around Hindu god Vishnu. Composed in Sanskrit, the earliest version of the text may have been composed in the 1st millennium CE, but it was likely expanded and changed over a long period of time.',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/gp/'
+  },
+  {
+    id: 'brahmanda',
+    name: 'Brahmanda Purana',
+    sanskritName: 'ब्रह्माण्ड पुराण',
+    description: 'The Purana describing the cosmic egg and universe',
+    details: 'The Brahmanda Purana is a Sanskrit text and one of the eighteen major Puranas. It is listed as the eighteenth Maha-Purana in almost all the anthologies. The text is named after one of the cosmological theories of Hinduism, namely the "Cosmic Egg" (Brahma-anda).',
+    source: 'Sacred Texts Archive',
+    sourceUrl: 'https://www.sacred-texts.com/hin/bp/'
+  }
+];
